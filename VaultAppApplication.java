bf1af61bd08cf38c436cca0da7965f02a9c7dbff
nvcvamsi <nvc.vamsi@gmail.com>
2024-10-15 09:59:48 +0100

    Create VaultAppApplication.java

---
+++ VaultAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.vaultapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultAppApplication.class, args);
+    }
+}
